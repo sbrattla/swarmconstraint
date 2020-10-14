6dbeb80a50ec74fb1d13600e6a0ef4cfb255d67c
Stian Brattland <stian@brattland.no>
2020-10-14 15:14:41 +0200

    Added support for configurations via file. Added better validation

--- swarmconstraint/swarmconstraint.py
+++ swarmconstraint/swarmconstraint.py
@@ -23,6 +23,23 @@ class SwarmConstraint:
     self.logger.setLevel(logging.DEBUG)
 
   def run(self):
+
+    if (not self.args['watch']):
+      raise Exception('At least one node to watch must be provided.')
+
+    if (not self.args['toggle']):
+      raise Exception('At least one node to toggle must be provided.')
+
+    if (not self.args['label']):
+      raise Exception('At least one label must be provided.')
+
+    if (not self.args['prefix']):
+      raise Exception('A prefix must be provided.')
+
+    self.logger.info('Watch {watch}.'.format(watch=','.join(self.args['watch'])))
+    self.logger.info('Toggle the label(s) {labels} on {toggle}.'.format(labels=','.join(self.args['label']), toggle=','.join(self.args['toggle'])))
+    self.logger.info('Prefix disabled labels with {prefix}.'.format(prefix=self.args['prefix']))
+
     # Collect availability for watched nodes, and keep track of the collective
     # availability for all the watched nodes.
     nodes = self.getNodes()
@@ -162,21 +179,36 @@ class SwarmConstraint:
 class FromFileAction(argparse.Action):
 
   def __init__(self, option_strings, dest, nargs=None, **kwargs):
-    if nargs is not None:
-      raise ValueError("nargs not allowed")
     super(FromFileAction, self).__init__(option_strings, dest, **kwargs)
 
-  def __call__(self, parser, namespace, values, option_string=None):
-    print('%r %r %r' % (namespace, values, option_string))
-    setattr(namespace, self.dest, values)
+  def __call__(self, parser, namespace, path, option_string=None):
+    if (path):
+      data = None
+      with open(path) as f:
+        data = json.load(f)
+
+      if (data is None):
+        return
+
+      if ('watch' in data):
+        namespace.watch += data['watch']
+
+      if (data['toggle']):
+        namespace.toggle += data['toggle']
+
+      if ('label' in data):
+        namespace.label += data['label']
+
+    return
 
 def main():
   parser = argparse.ArgumentParser(description='Toggles one or more constraints depending on node availability')
   parser.add_argument('--watch', metavar='watch', action='append', default=[], help='A node which availability is to be watched.')
   parser.add_argument('--toggle', metavar='toggle', action='append', default=[], help='A node for which constraints are to be toggled. Defaults to all nodes.')
-  parser.add_argument('--label', metavar='label', action='append', help='A label which is to be toggled according to availability for watched nodes.')
+  parser.add_argument('--label', metavar='label', action='append', default=[], help='A label which is to be toggled according to availability for watched nodes.')
   parser.add_argument('--prefix', metavar='prefix', default='disabled',  help='The prefix to use for disabled labels. Defaults to "disabled".')
-  parser.add_argument('--from-file', metavar='fromFile', action=FromFileAction, help='A file which holds configurations.')
+  parser.add_argument('fromFile', action=FromFileAction, help='A file which holds configurations.')
+
   args = vars(parser.parse_args())
   se = SwarmConstraint(args)
 
@@ -185,6 +217,9 @@ def main():
       se.run()
       time.sleep(10)
     except KeyboardInterrupt:
+      break
+    except Exception as err:
+      print(err)
       break
 
 if __name__ == '__main__':
